28ef11276c8451ded1d6784987061ec542975400
Emerson Rocha <metalianlord@hotmail.com>
2021-01-22 17:08:57 -0300

    Adicionado arquivos do capítulo de Arrays

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Waleskah', 'Cibalena']
+const filhos = ['Woxiton', 'Wesclei']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3,4], 5, [[6,7]]))
